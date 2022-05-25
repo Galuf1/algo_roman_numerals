709e83ab3b64f1d03abc00d311892fc10123b341
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-25 21:06:29 +0200

    translated from js

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,37 @@
 def to_roman(num):
-    # write your code here!
+    output = ""
+
+    roman_to_arabic = {
+        "I":1,
+        "IV":4,
+        "V":5,
+        "IX":9,
+        "X":10,
+        "XL":40,
+        "VL":45,
+        "IL":49,
+        "L":50,
+        "XC":90,
+        "VC":95,
+        "IC":99,
+        "C":100,
+        "CD":400,
+        "LD":450,
+        "XD":490,
+        "VD":495,
+        "ID":499,
+        "D":500,
+        "CM":900,
+        "LM":950,
+        "XM":990,
+        "VM":995,
+        "IM":999,
+        "M":1000,
+    }
+    
+    listu = roman_to_arabic.keys()
+    for i in reversed(listu):
+        while num >= roman_to_arabic[i]:
+            output += i
+            num -= roman_to_arabic[i]
+    return output
