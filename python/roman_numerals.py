ac05cce20c7db6438bc455cd2eed24815fa077f0
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-02 14:29:56 +0200

    solved with div-remainder

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -8,30 +8,23 @@ def to_roman(num):
         "IX":9,
         "X":10,
         "XL":40,
-        "VL":45,
-        "IL":49,
         "L":50,
         "XC":90,
-        "VC":95,
-        "IC":99,
         "C":100,
         "CD":400,
-        "LD":450,
-        "XD":490,
-        "VD":495,
-        "ID":499,
         "D":500,
         "CM":900,
-        "LM":950,
-        "XM":990,
-        "VM":995,
-        "IM":999,
         "M":1000,
     }
     
-    listu = roman_to_arabic.keys()
-    for i in reversed(listu):
-        while num >= roman_to_arabic[i]:
-            output += i
-            num -= roman_to_arabic[i]
+    # listu = roman_to_arabic.keys()
+    # for i in reversed(listu):
+    #     while num >= roman_to_arabic[i]:
+    #         output += i
+    #         num -= roman_to_arabic[i]
+
+    for arab in reversed(roman_to_arabic):
+        d, m = divmod(num,roman_to_arabic[arab])
+        output += arab * d
+        num = m
     return output
